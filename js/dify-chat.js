539f67e11eba2a840a53b5a29761c70b3e9cb6ff
Badge5426 <17350881826@163.com>
2024-10-23 21:46:24 +0800

    Site updated: 2024-10-23 21:46:24

--- js/dify-chat.js
+++ js/dify-chat.js
@@ -1,11 +1,11 @@
 // 聊天机器人配置
 window.difyChatbotConfig = {
-    token: '49uQsorBHd3oimlq',
-    baseUrl: 'http://121.41.7.120'
+    token: 'UGjiAhQ4tpVvJzo7',
+    baseUrl: 'https://udify.app'
   };
   
 // 聊天机器人脚本
-  document.write('<script src="http://121.41.7.120/embed.min.js" id="49uQsorBHd3oimlq" defer></script>');
+  document.write('<script src="https://udify.app/embed.min.js" id="UGjiAhQ4tpVvJzo7" defer></script>');
   
 // 聊天机器人样式
   document.write('<style> #dify-chatbot-bubble-button { background-color: #1C64F2 !important; } </style>');
