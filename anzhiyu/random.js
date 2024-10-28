c60188c63f819de0f130c5f582af559199f34487
Badge5426 <17350881826@163.com>
2024-10-28 19:04:28 +0800

    Site updated: 2024-10-28 19:04:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,4 +1,4 @@
-var posts=["posts/2c10.html","posts/84b1.html","posts/bf2b.html","posts/28d2.html","posts/3eeb.html"];function toRandomPost(){
+var posts=["posts/2c10.html","posts/3566.html","posts/3a37.html","posts/84b1.html","posts/bf2b.html","posts/28d2.html","posts/3eeb.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };var friend_link_list=[];
     var refreshNum = 1;
